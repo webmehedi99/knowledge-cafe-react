31e1d1e5a206d29a3a4688baf04754671cc599c9
webmehedi99 <webmehedi99@gmail.com>
2024-10-22 23:15:40 +0600

    create blog, bookmark and another important part

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Components/Header/Header'
+import Blogs from './Components/Blogs/Blogs'
+import Bookmarks from './Components/Bookmarks/Bookmarks'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
-      <h1 className='text-6xl bg-red-700 text-center'>Knowledge Cafe</h1>
-      
-      
+      <Header></Header>
+      <div className='md:flex'>
+        <Blogs></Blogs>
+        <Bookmarks></Bookmarks>
+      </div>
     </>
   )
 }
---
+++ src/Components/Blog/Blog.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+
+const Blog = ({blog}) => {
+    console.log(blog);
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+Blog.propTypes ={
+    blog : PropTypes.object.isRequired
+}
+
+
+export default Blog;
